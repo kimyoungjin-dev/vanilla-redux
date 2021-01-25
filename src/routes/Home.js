0a50106e50ac0cb80a1d70bbbb5005ceb6774630
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-25 22:39:25 +0900

    Make mapStateToProps and mapDispatchToProps functions

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import ToDo from "../components/ToDo";
+import { actionCreators } from "../store";
 
-const Home = () => {
+const Home = ({ todos, addTodo }) => {
   const [text, setText] = useState("");
 
   const onChange = (event) => {
@@ -12,7 +15,7 @@ const Home = () => {
 
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log(text);
+    addTodo(text);
     setText("");
   };
 
@@ -28,9 +31,23 @@ const Home = () => {
         />
         <input type="submit" value="todo" />
       </form>
-      <ul></ul>
+      <ul>
+        {todos.map((todo) => (
+          <ToDo {...todo} />
+        ))}
+      </ul>
     </div>
   );
 };
+const mapStateToProps = (state, ownProps) => {
+  return { todos: state };
+};
 
-export default Home;
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    addTodo: (text) => dispatch(actionCreators.addTodo(text)),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
+// mapStateToProps redux state 로부터 home 에 props를 전달해준다.
+//connect는 stated에서 home으로 보내는 props 에 추가될수있도록 허용해준다.
